3f529b0fb04e0dd0e0130bc6a94e68f38767de73
rafialfatih <rafi.alfatih@gmail.com>
2023-04-22 19:26:00 +0700

    Add welcome message notification

---
+++ app/Notifications/WelcomeMessage.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeMessage extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(
+      protected readonly string $name
+    )
+    {}
+
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->greeting('Welcome, ' . $this->name . '!')
+                    ->line('The introduction to the notification.')
+                    ->line('To start using our application, click button below.')
+                    ->action('Click here!', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
