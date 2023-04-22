4664b3d9fb2e2a8eaa24af850971eedcd4e9af76
rafialfatih <rafi.alfatih@gmail.com>
2023-04-22 19:25:31 +0700

    Add user store request

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required'],
+            'password' => ['required']
+        ];
+    }
+
+    public function userData(): array
+    {
+        $data = $this->validated();
+
+        $data['password'] = Hash::make($data['password']);
+
+        return $data;
+    }
+}
